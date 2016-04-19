5e59c2e90912ce0f0e833bc2d530b906ff1ea1ac
ogaksk <ogawa@ogawa-air.local>
2016-04-19 19:14:46 +0900

    first

---
+++ routes/index.js
@@ -0,0 +1,50 @@
+var express = require('express'),
+    request = require('request'),
+    router = express.Router(),
+    token = "<your-access-token>";
+
+function sendTextMessage(sender, text) {
+  var messageData = {
+    text:text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
+
+router.get('/', function(req, res, next) {
+  if (req.query['hub.verify_token'] === '<validation_token>') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+});
+
+router.post('/', function(req, res, next) {
+  var messaging_events = req.body.entry[0].messaging,
+      replayMessages = [];
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+
+      // アレコレしたいことをどうぞ
+      sendTextMessage(sender, text.substring(0, 200));
+    }
+  }
+  res.sendStatus(200);
+});
+
+module.exports = router;
